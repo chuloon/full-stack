5cd3a2e09881454ea8b0c56c56077f7eec2fbee5
chuloon <ben.ng@outlook.com>
2017-02-19 12:00:40 -0500

    added styles

--- scripts/prime.js
+++ scripts/prime.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
     ko.applyBindings(new primeNumberViewModel());
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-center",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
 });
 
 isPrime = (num) => {
@@ -18,10 +36,36 @@ isPrime = (num) => {
     return true;
 }
 
+ko.validation.init({
+    grouping: {
+        deep: true,
+        live: true,
+        observable: true
+    }
+});
+
 function primeNumberViewModel() {
-    this.startingValue = ko.observable(1);
-    this.endingValue = ko.observable(5);
+    this.inputFields = {
+        startingValue: ko.observable().extend({ required: true, number: true }),
+        endingValue: ko.observable().extend({ required: true, number: true })
+    };
+    //this.startingValue = ko.observable().extend({ required: true });
+    //this.endingValue = ko.observable(5).extend({ required: true });
     this.primeList = ko.observable([]);
+
+    this.errors = ko.validation.group(this.inputFields);
+
+    ko.validation.rules.pattern.message = 'Invalid.';
+
+    this.submitClick = (startingValue, endingValue) => {
+        if (this.errors().length > 0) {
+            this.errors.showAllMessages();
+            toastr["error"]("Invalid fields detected");
+            return;
+        }
+
+        this.generate(startingValue, endingValue);
+    }
 
     this.generate = (startingValue, endingValue) => {
         let primeList = [];
